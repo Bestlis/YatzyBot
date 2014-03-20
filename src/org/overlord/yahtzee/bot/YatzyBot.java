6867ed721df910fe64ee0996f54a74aeb1c841c4
Chris Dennett <dessimat0r@gmail.com>
2014-03-20 15:44:55 +0000

    Update to 0.863, fixed help notice.

--- src/org/overlord/yahtzee/bot/YatzyBot.java
+++ src/org/overlord/yahtzee/bot/YatzyBot.java
@@ -38,7 +38,7 @@ import org.pircbotx.hooks.events.MessageEvent;
 import org.pircbotx.hooks.events.PartEvent;
 
 public class YatzyBot {
-	protected static final String VERSION = "0.862";
+	protected static final String VERSION = "0.863";
 	
 	protected final YatzyUser user;
 	protected final String server;
